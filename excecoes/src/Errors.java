8df886058bbf297f5ed0771dd8302c319cccc909
Jorge Tomé <jorge.tomejr@outlook.com>
2025-05-14 13:45:19 -0400

    Fundamentos: excecções( try-catch/finally)

---
+++ excecoes/src/Errors.java
@@ -0,0 +1,27 @@
+public class Errors {
+    public static void main(String[] args) {
+        //1 - try catch (erro de divisão por zero)
+        try {
+            int a = 0;
+            int b = 10;
+
+            int resultado = b / a;
+
+        }catch (ArithmeticException e){
+            System.out.println("Divisão por zero não é possível");
+            System.out.println("Msg: " + e.getMessage());
+        }
+
+        //erro de índice não existente
+        try {
+            int[] numeros = {1,2,3};
+            System.out.println("Acessando número existente: " + numeros[2]);
+            //System.out.println(numeros[3]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Msg: " + e.getMessage());
+        } finally {
+            System.out.println("Executou o finally");
+        }
+    }
+}
